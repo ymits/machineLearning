c49a0cb890685ddd95d9e343a955db91f7431bd6
Mitsui Yasuyuki <mitsui.y.k@gmail.com>
2017-12-28 03:07:01 +0900

    1.3まで

--- collective_intelligence_programing/2_recommend/recommendations.py
+++ collective_intelligence_programing/2_recommend/recommendations.py
@@ -67,28 +67,63 @@ def sim_distance(prefs, person1, person2):
     
     return 1 / (1 + sqrt(sum_of_squares))
 
-def sim_pearson(perfs, person1, person2):
+def sim_pearson(prefs, person1, person2):
     # 二人共評価しているアイテムのリストを得る
     si = {}
-    for item in perfs[person1]:
-        if item in perfs[person2]:
+    for item in prefs[person1]:
+        if item in prefs[person2]:
             si[item] = 1
             
     n = len(si)
     if n == 0:
         return 0
     
-    avg1 = sum([perfs[person1][item] for item in si]) / n
-    avg2 = sum([perfs[person2][item] for item in si]) / n
-    covariance = sum([(perfs[person1][item] - avg1) * (perfs[person2][item] - avg2) for item in si]) / n
-    stdev1 = sqrt(sum([(perfs[person1][item] - avg1) ** 2 for item in si]) / n)
-    stdev2 = sqrt(sum([(perfs[person2][item] - avg2) ** 2 for item in si]) / n)
+    avg1 = sum([prefs[person1][item] for item in si]) / n
+    avg2 = sum([prefs[person2][item] for item in si]) / n
+    covariance = sum([(prefs[person1][item] - avg1) * (prefs[person2][item] - avg2) for item in si]) / n
+    stdev1 = sqrt(sum([(prefs[person1][item] - avg1) ** 2 for item in si]) / n)
+    stdev2 = sqrt(sum([(prefs[person2][item] - avg2) ** 2 for item in si]) / n)
     
     return covariance / (stdev1 * stdev2)
     
-def topMatches(perfs, person, n = 5, similarity = sim_pearson):
-    scores = [(similarity(perfs, person, other), other) for other in perfs if person != other]
+def topMatches(prefs, person, n = 5, similarity = sim_pearson):
+    scores = [(similarity(prefs, person, other), other) for other in prefs if person != other]
     
     scores.sort()
     scores.reverse()
     return scores[0:n]
+
+def getRecommendations(prefs, person, similarity = sim_pearson):
+    matches = topMatches(prefs, person)
+    sims = {}
+    for match in matches:
+        sims[match[1]] = match[0]
+        
+    totals = {}
+    simsum = {}
+
+    for other in sims:
+        for item in prefs[other]:
+            if item not in prefs[person] or prefs[person][item] == 0:
+                if sims[other] < 0 :
+                    continue
+                
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sims[other]
+
+                simsum.setdefault(item, 0)
+                simsum[item] += sims[other]
+                
+    rankings = [(totals[item] / simsum[item], item) for item in totals]
+
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+def transformPrefs(prefs):
+    result={}
+    for person in prefs:
+        for item in prefs[person]:
+            result.setdefault(item, {})
+            result[item][person] = prefs[person][item]
+    return result
